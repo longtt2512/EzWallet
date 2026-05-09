fa53463e6241194e874905631684bb9b17153248
Long Truong <longtt2512@gmail.com>
2026-05-09 23:57:56 +0700

    Initial commit

--- backend/build/generated/sources/annotationProcessor/java/main/com/ezwallet/module/auth/mapper/UserMapperImpl.java
+++ backend/build/generated/sources/annotationProcessor/java/main/com/ezwallet/module/auth/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-09T16:26:35+0700",
+    date = "2026-05-09T23:48:30+0700",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
@@ -26,6 +26,7 @@ public class UserMapperImpl implements UserMapper {
         String email = null;
         String phone = null;
         String fullName = null;
+        String avatarUrl = null;
         UserStatus status = null;
         UserTier tier = null;
 
@@ -34,10 +35,11 @@ public class UserMapperImpl implements UserMapper {
         email = user.getEmail();
         phone = user.getPhone();
         fullName = user.getFullName();
+        avatarUrl = user.getAvatarUrl();
         status = user.getStatus();
         tier = user.getTier();
 
-        UserProfileDto userProfileDto = new UserProfileDto( id, username, email, phone, fullName, status, tier );
+        UserProfileDto userProfileDto = new UserProfileDto( id, username, email, phone, fullName, avatarUrl, status, tier );
 
         return userProfileDto;
     }
--- backend/build/generated/sources/annotationProcessor/java/main/com/ezwallet/module/topup/mapper/BankAccountMapperImpl.java
+++ backend/build/generated/sources/annotationProcessor/java/main/com/ezwallet/module/topup/mapper/BankAccountMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-09T16:26:35+0700",
+    date = "2026-05-09T23:48:30+0700",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
